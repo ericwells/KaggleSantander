82151ba13e3da2ca9c77197fb79f483238b219c1
Eric Wells <ericwells@gmail.com>
2016-12-16 10:47:47 -0800

    This version now has a successfully working logistic regression (and predict function) for one product. Now need to expand to all 24 products.

--- logistic_regressions.R
+++ logistic_regressions.R
@@ -127,15 +127,11 @@ randomRows = function(df,n){
   return(df[sample(nrow(df),n),])
 }
 
-# just some quick test of the modeling sytnax
-#model11 <- lm(df4[,408] ~ df4[,2] + df4[,3]) # Oddly, this ran when I tried it a second time....  Could try it again using header_names[6] approach.
-#model11 <- lm(df4$'ind_ahor_fin_ult1.2016-05-28' ~ df4$`segmento.2015-01-28` + df4$`cod_prov.2015-01-28`) # this was a test of syntax and it ran.
-#junk <- predict(model11, newdata = data.frame(df4$`segmento.2015-01-28`, df4$`cod_prov.2015-01-28`)) # this was a test of syntax and it ran. Named num [1:926663]
-
 # Build a training dataset using new names so that the predict function won't get confused.
 #saving this version for when I build more predictors off the whole 12 month history... training <- data.frame(df4_samp[,c(1,408:431,2:23,384:407)]) # this is ncodpers, then 24 targets, then 22 demographic predictors, then 24 product indicator predictors. 71 variables.
 set.seed(42)
-training <- randomRows(df4[,c(1:71)],10000) # this is ncodpers, then 24 targets, then 22 demographic predictors, then 24 product indicator predictors. 71 variables.
+training <- randomRows(df4[,c(1:71)],100000) # this is ncodpers, then 24 targets, then 22 demographic predictors, then 24 product indicator predictors. 71 variables.
+# TODO:  once all the code is working, bump the sample size up to 100K.
 library(plyr)
 training <- rename(training, c(
   "ncodpers"="ncodpers", 
@@ -144,9 +140,7 @@ training <- rename(training, c(
   "ind_ahor_fin_ult1.2016-04-28"="P1", "ind_aval_fin_ult1.2016-04-28"="P2", "ind_cco_fin_ult1.2016-04-28"="P3", "ind_cder_fin_ult1.2016-04-28"="P4", "ind_cno_fin_ult1.2016-04-28"="P5", "ind_ctju_fin_ult1.2016-04-28"="P6", "ind_ctma_fin_ult1.2016-04-28"="P7", "ind_ctop_fin_ult1.2016-04-28"="P8", "ind_ctpp_fin_ult1.2016-04-28"="P9", "ind_deco_fin_ult1.2016-04-28"="P10", "ind_deme_fin_ult1.2016-04-28"="P11", "ind_dela_fin_ult1.2016-04-28"="P12", "ind_ecue_fin_ult1.2016-04-28"="P13", "ind_fond_fin_ult1.2016-04-28"="P14", "ind_hip_fin_ult1.2016-04-28"="P15", "ind_plan_fin_ult1.2016-04-28"="P16", "ind_pres_fin_ult1.2016-04-28"="P17", "ind_reca_fin_ult1.2016-04-28"="P18", "ind_tjcr_fin_ult1.2016-04-28"="P19", "ind_valo_fin_ult1.2016-04-28"="P20", "ind_viv_fin_ult1.2016-04-28"="P21", "ind_nomina_ult1.2016-04-28"="P22", "ind_nom_pens_ult1.2016-04-28"="P23", "ind_recibo_ult1.2016-04-28"="P24"                          
    ))
 
-# write out to CSV in case R crashes then I don't have to do read a huge file in again.  BAD!!! Don't ever do this again as the write/read messed with the data!
-#write.csv(training, file = "../training.csv", row.names = FALSE, quote = FALSE) 
-#training <- read.csv("../training.csv", stringsAsFactors=FALSE, header = TRUE)
+# Note to self: remember to save the workspace at this point in the code.
 
 # And now build a scoring dataset that has appropriate names that the predict function expects.
 # All I need from test_ver2 is the list of IDs.  The predictors all come from months that are found on train_ver2.  Therefore I can build scoring from df4 (which was already merged against test_ver2 IDs).
@@ -157,14 +151,50 @@ scoring <- rename(scoring, c(
   "ind_ahor_fin_ult1.2016.05.28"="P1", "ind_aval_fin_ult1.2016.05.28"="P2", "ind_cco_fin_ult1.2016.05.28"="P3", "ind_cder_fin_ult1.2016.05.28"="P4", "ind_cno_fin_ult1.2016.05.28"="P5", "ind_ctju_fin_ult1.2016.05.28"="P6", "ind_ctma_fin_ult1.2016.05.28"="P7", "ind_ctop_fin_ult1.2016.05.28"="P8", "ind_ctpp_fin_ult1.2016.05.28"="P9", "ind_deco_fin_ult1.2016.05.28"="P10", "ind_deme_fin_ult1.2016.05.28"="P11", "ind_dela_fin_ult1.2016.05.28"="P12", "ind_ecue_fin_ult1.2016.05.28"="P13", "ind_fond_fin_ult1.2016.05.28"="P14", "ind_hip_fin_ult1.2016.05.28"="P15", "ind_plan_fin_ult1.2016.05.28"="P16", "ind_pres_fin_ult1.2016.05.28"="P17", "ind_reca_fin_ult1.2016.05.28"="P18", "ind_tjcr_fin_ult1.2016.05.28"="P19", "ind_valo_fin_ult1.2016.05.28"="P20", "ind_viv_fin_ult1.2016.05.28"="P21", "ind_nomina_ult1.2016.05.28"="P22", "ind_nom_pens_ult1.2016.05.28"="P23", "ind_recibo_ult1.2016.05.28"="P24"
 ))
 
-# Now run a logistic regression on the training dataset to predict just Product 1 (target=T1) using various predictors from demographics and product.
-#my_formula <- as.factor(T3)~D1+D2+D3+D4+D5+D6+D7+D8+D9+D10+D11+D12+D13+D14+D15+D16+D17+D18+D19+D20+D21+D22+P1+P2+P3+P4+P5+P6+P7+P8+P9+P10+P11+P12+P13+P14+P15+P16+P17+P18+P19+P20+P21+P22+P23+P24
-#my_formula <- as.factor(T3)~D3+D4 # a baby model to test the syntax. D3 is Sexo, D4 is Age. 
-my_formula <- as.factor(T3)~D3+D4+D7+D15+D21 # a model to test what happens if just using the good demographic predictors. D3 is Sexo, D4 is Age, D7 is Antiguedad (time as customer), D15 is canal-entrada (channel), D21 is renta (income).
-model12 <- glm(formula = my_formula, family = binomial(link = "logit"), data = training, trace=TRUE, maxit=25, na.action=na.omit)
-summary(model12)
+# TODO:  May need to strip out the products that don't have enough data in the target.  Could then basically assume their prob is zero.
+# let's take a quick check...
+table(training[,2])
+for (i in 2:25){
+  print(paste("T",i-1,table(df4[,i])))
+}
+# the only Targets that have interesting counts are T3, T8, and T24.  
+# definitely teeny counts are T1, T2, T4, T10, T11.
+# so I am going to try logistic regressions only on T3, T5, T8, T9, T12, T13, T14, T18, T19, T20, T22, T23, T24.
+# [1] "T 1 929537" "T 1 78"    
+# [1] "T 2 929599" "T 2 16"    
+# [1] "T 3 368004" "T 3 561611"
+# [1] "T 4 929299" "T 4 316"   
+# [1] "T 5 856554" "T 5 73061" 
+# [1] "T 6 922034" "T 6 7581"  
+# [1] "T 7 921518" "T 7 8097"  
+# [1] "T 8 829026" "T 8 100589"
+# [1] "T 9 896344" "T 9 33271" 
+# [1] "T 10 929298" "T 10 317"   
+# [1] "T 11 928594" "T 11 1021"  
+# [1] "T 12 898398" "T 12 31217" 
+# [1] "T 13 853772" "T 13 75843" 
+# [1] "T 14 914928" "T 14 14687" 
+# [1] "T 15 925087" "T 15 4528"  
+# [1] "T 16 922256" "T 16 7359"  
+# [1] "T 17 927624" "T 17 1991"  
+# [1] "T 18 884116" "T 18 45499" 
+# [1] "T 19 894793" "T 19 34822" 
+# [1] "T 20 908189" "T 20 21426" 
+# [1] "T 21 926656" "T 21 2959"  
+# [1] "T 22 881311" "T 22 48304" 
+# [1] "T 23 876772" "T 23 52843" 
+# [1] "T 24 816600" "T 24 113015"
+# In retrospect, it probably would have been a better approach to create a target variable of "added this product in the target month" rather than the approach I chose (dedup against the product set they aleady have *after* making the prediction).
 
-# This is where I left off at Thu Dec 15 17:20:22 PST 2016.
+# IMPROVEMENT: many of the target variables have heavily skewed counts of 1 vs 0.  could probably do better if we sampled differently - a stratified random sample.
+
+# Now run a logistic regression on the training dataset to predict just one product using various predictors from demographics and product.
+#my_formula <- as.factor(T3)~D3+D4 # a baby model to test the syntax. D3 is Sexo, D4 is Age. 
+#my_formula <- as.factor(T3)~D3+D4+D7+D15+D21 # a model to test what happens if just using the good demographic predictors. D3 is Sexo, D4 is Age, D7 is Antiguedad (time as customer), D15 is canal-entrada (channel), D21 is renta (income).
+#my_formula <- as.factor(T3)~D3+D4+D7+D15+D21+P1+P2+P4+P5+P6+P7+P8+P9+P10+P11+P12+P13+P14+P15+P16+P17+P18+P19+P20+P21+P22+P23+P24 # a model to test what happens if just using the good demographic predictors. D3 is Sexo, D4 is Age, D7 is Antiguedad (time as customer), D15 is canal-entrada (channel), D21 is renta (income).
+my_formula <- as.factor(T3)~D4+D7+D15+P5+P8+P9+P11+P12+P13+P14+P18+P19+P20+P21+P22+P23+P24 # a model to test what happens if just using the good demographic predictors. D3 is Sexo, D4 is Age, D7 is Antiguedad (time as customer), D15 is canal-entrada (channel), D21 is renta (income).
+model12_T3 <- glm(formula = my_formula, family = binomial(link = "logit"), data = training, trace=TRUE, maxit=25, na.action=na.omit)
+summary(model12_T3)
 
 # In order to get the predict function to work, need to check for any factor levels that didn't exist in training and convert to NA.
 # I think I can skip this step now...
@@ -172,13 +202,24 @@ summary(model12)
 #scoring$D1[id] <- NA
 
 # And now try predict syntax
-junk <- predict(model12, newdata = scoring, type="response", na.action=na.omit)
+PrT3 <- predict(model12_T3, newdata = scoring, type="response", na.action=na.omit)
+# TODO: I'll need to have 24 predictions vectors here.  Then I'll stitch them together into a dataframe for the recommendation sorting/deduping phase.
 
-# TODO: need to check if I have enough rows in the output.  Should be 929615.
+# need to check if I have enough rows in the output.  Should be 929615.
+length(PrT3)
+# Yes it does.
 
 # TODO: confirm that the predict function output 24 probabilities between 1 and 24.
+head(PrT3,n=50L)
+summary(PrT3)
+# Yes, for T3 they are in range 0-1.
+
+# TODO: figure out how to run all 24 predictions and get them in the right place.
+# My initial thought here is that each of the 24 predictions ultimately be selecting different predictors.  Even though is will add many lines of code, perhaps it would be good to build each of the 24 models manually.
+# So could have "model12_T1", "model12_T2", ..., "model12_T24".
 
 # TODO: consider bringing in the stuff that creates the Kaggle submission file.
+# get from ksb5and6and7.R
 
 ### SCRATCHPAD
 df[df$ncodpers==1050934,c(1:5,783)]
